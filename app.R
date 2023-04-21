494a621718f5a133e17b3641638c004b703855f9
willgertsch <willgertsch+github@gmail.com>
2023-04-20 17:12:24 -0700

    added some explanatory text on what lambda is

--- app.R
+++ app.R
@@ -369,7 +369,7 @@ the relevant optimal design can be found using the framework of c-optimality.
                        column(
                          6,
                          textInput("theta_input_bmd", "Theta ( enter values separated by , )"),
-                         numericInput("lambda_input", "\\(\\lambda\\)",
+                         numericInput("lambda_input", "\\(\\lambda\\) (weight parameter for D vs. c optimality)",
                                       value = 0.5, min = 0, max = 1, step = 0.1)
                        )
                      ),
